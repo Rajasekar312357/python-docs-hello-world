4dd4ef596ce1ef65c9991de893dad56ee5b5bf99
Rajasekar312357 <66265779+Rajasekar312357@users.noreply.github.com>
2020-06-10 22:59:26 -0400

    Update application.py

--- application.py
+++ application.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 def main():
     
     q1 = request.form.get("q")
-    
+    response1 = ''
     if q1 is not None:
         es = Elasticsearch(["https://elastic:EoDnxU5mTG90AT1tgO7uFUH4@e67c41f68a584eaead9f9108bc9e0800.us-east-1.aws.found.io:9243"])
         resp = es.search(index='kibana_sample_data_flights',doc_type='_doc',body={"query": {"match": {"Carrier": q1}}})
         for doc in resp['hits']['hits']:
-            (doc['_id'],doc['_source']['FlightNum'])
-        return render_template('index1.html',q=q1,response=(doc['_id'],doc['_source']['FlightNum']))
+            response1 = (doc['_id'],doc['_source']['FlightNum'])
+        return render_template('index1.html',q=q1,response=response1)
     
     return render_template('index1.html')
 
